5d9d1391e74f5cf60f3e7e66048b8aae3696172e
izabelladudaryk <izabelladudaryk@gmail.com>
2021-07-21 22:32:22 +0300

    initial commit

---
+++ app/src/main/java/com/disabella/mylogin/LoginActivity.kt
@@ -0,0 +1,47 @@
+package com.disabella.mylogin
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val login = findViewById<TextInputLayout>(R.id.loginLayout)
+        val password = findViewById<TextInputLayout>(R.id.passwordLayout)
+        val sharedPref = getSharedPreferences("main", Context.MODE_PRIVATE)
+
+        @SuppressLint("CommitPrefEdits")
+        fun validate() {
+            login.error = if (login.editText?.text.toString() != "admin") {
+                "this Login is invalid"
+            } else {
+                sharedPref.edit().putString("login", login.editText?.text.toString())
+                null
+            }
+            password.error = if (password.editText?.text.toString() != "pass") {
+                "this Password is invalid"
+            } else {
+                sharedPref.edit().putString("password", password.editText?.text.toString())
+                null
+            }
+        }
+
+        password.editText?.setOnEditorActionListener { _, actionId, _ ->
+            return@setOnEditorActionListener when (actionId) {
+                EditorInfo.IME_ACTION_DONE -> {
+                    validate()
+                    startActivity(Intent(this, MainActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/disabella/mylogin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.disabella.mylogin
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongConstant")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textLogin = findViewById<TextView>(R.id.textViewLogin)
+        val textPassword = findViewById<TextView>(R.id.textViewPassword)
+
+        textLogin.text = getSharedPreferences("login", 1).toString()
+        textPassword.text = getSharedPreferences("password", 1).toString()
+    }
+}
